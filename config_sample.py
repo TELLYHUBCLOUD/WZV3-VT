224af709c86da17c7ac58dd3bb38e62d27d32197
mari8438 <mari8438ganesh@gmail.com>
2026-06-24 18:47:31 +0530

    Update upstream repository and branch in config
    Signed-off-by: mari8438 <mari8438ganesh@gmail.com>

--- config_sample.py
+++ config_sample.py
@@ -281,8 +281,8 @@ USENET_SERVERS = [
 ]
 
 # Update
-UPSTREAM_REPO = ""
-UPSTREAM_BRANCH = "master"
+UPSTREAM_REPO = "https://github.com/mari8438/WZML-X"
+UPSTREAM_BRANCH = "starfallx-v1.2-video-tools"
 UPDATE_PKGS = True
 
 # Leech

e99121790fae4d95c4409fae131201248c08db89
mari8438 <mari8438ganesh@gmail.com>
2026-07-23 13:35:30 +0530

    Fix SABnzbd config path

--- bot/helper/ext_utils/db_handler.py
+++ bot/helper/ext_utils/db_handler.py
@@ -108,7 +108,7 @@ class DbManager:
     async def update_nzb_config(self):
         if self._return:
             return
-        async with aiopen("sabnzbd/SABnzbd.ini", "rb+") as pf:
+        async with aiopen("configs/sabnzbd/SABnzbd.ini", "rb+") as pf:
             nzb_conf = await pf.read()
         await self.db.settings.nzb.replace_one(
             {"_id": self._partition()}, {"SABnzbd__ini": nzb_conf}, upsert=True
